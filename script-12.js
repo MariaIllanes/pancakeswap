21cb3a1bb2c231777925958f0c07436be17a7821
María Illanes <45496091+MariaIllanes@users.noreply.github.com>
2023-11-11 12:40:54 +0100

    Update script-12.js
    solucionar: añadir libreria idiomas + solucionar selects (da igual en el que cliques, solo se abre el de telegram pero no logro encontrar el error de js)

--- script-12.js
+++ script-12.js
@@ -1,18 +1,77 @@
-// const sun = document.getElementById("icon-sun");
+const toggleCheckbox = document.getElementById('toggleCheckbox');
+        const footerContainer = document.querySelector('.footer-container, .exchange-stats2-container'); //añadir aqui las clases de las secciones que se ven afectadas y quitar .footer-container. concretar colores
 
-// sun.addEventListener("change", function(){
-//     if ()
-// }) 
+        toggleCheckbox.addEventListener('change', function () {
+            if (toggleCheckbox.checked) {
+                footerContainer.style.backgroundColor = '#27262c';
+                footerContainer.style.color = '#ccc';
+
+            } else {
+                footerContainer.style.backgroundColor = '#ccc';
+                footerContainer.style.color = '#27262c';
+            }
+        });
 
 
-const bgToggle = document.getElementById("switch");
 
-    bgToggle.addEventListener("change", function() {
-        const body = document.body;
 
-        if (bgToggle.checked) {
-            body.style.backgroundColor = "darkblue";
-        } else {
-            body.style.backgroundColor = "ligtblue";
-        }
-    });
+
+
+
+
+
+
+
+// language select piece of code
+        const selectLanguageIcon = document.getElementById('icon-language', 'select-language-label');
+        const selectLanguage = document.getElementById('hidden-language-menu');
+        
+        let isOpen = false;
+        
+        selectLanguageIcon.addEventListener('click', () => {
+          if (isOpen) {
+            selectLanguage.classList.remove('open');
+          } else {
+            selectLanguage.classList.add('open');
+          }
+          isOpen = !isOpen;
+        });
+        
+        document.addEventListener('click', (event) => {
+          if (!selectLanguage.contains(event.target) && event.target !== selectLanguageIcon) {
+            selectLanguage.classList.remove('open');
+            isOpen = false;
+          }
+        });
+
+
+
+
+        // telegram select piece of code
+
+        const selectTelegramLogo = document.getElementById('logo-telegram');
+        const selectLanguageTelegram = document.getElementById('language-options-telegram');
+        
+        let isOpen2 = false;
+        
+        selectTelegramLogo.addEventListener('click', () => {
+          if (isOpen2) {
+            selectLanguageTelegram.classList.remove('open');
+          } else {
+            selectLanguageTelegram.classList.add('open');
+          }
+          isOpen2 = !isOpen2;
+        });
+        
+        document.addEventListener('click', (event) => {
+          if (!selectLanguageTelegram.contains(event.target) && event.target !== selectIcon) {
+            selectLanguageTelegram.classList.remove('open');
+            isOpen2 = false;
+          }
+        });
+
+
+
+
+
+
