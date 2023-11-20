80b97418867fe2f92be33cf4edead486d107c9bf
María <m-i-y@live.com>
2023-11-20 20:00:16 +0100

    section finished

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import { buyCakeDocComponent } from "./buy-cake.js";
+import { mainBannerDocComponent } from "./main-banner.js";
+import { themeChange } from "./themchange.js";
+import {exchangeStatsDocComponents} from "./exchange-stats2.js";
+
+
+
+mainBannerDocComponent();
+themeChange();
+buyCakeDocComponent();
+exchangeStatsDocComponents();
