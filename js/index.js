0e24aaeabd8ac6fa71f85c592a747075d815999f
dmg310802 <danielmtz@next2.com.mx>
2023-11-21 23:39:34 +0100

    feat: added sec9 js to index, img does not work need to export js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ import { mainBannerDocComponent } from "./main-banner.js";
 import { themeChange } from "./themchange.js";
 import { mainNavDocComponent } from "./main-navbar.js";
 import { communityDocComponents } from "./community.js";
+import { partnersAnimationDocComponent } from "./partners-animation.js";
 
 
-
+partnersAnimationDocComponent();
 mainBannerDocComponent();
 themeChange();
 buyCakeDocComponent();
