fd7d7a2ce4193d2559233a61efe663e1b03dc763
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-11-22 00:22:17 +0100

    Update index.html, index.js and script-for-s4.js.

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ import { mainBannerDocComponent } from "./main-banner.js";
 import { themeChange } from "./themchange.js";
 import { mainNavDocComponent } from "./main-navbar.js";
 import { communityDocComponents } from "./community.js";
-
-
+import { exchange } from "./exchange.js";
 
 mainBannerDocComponent();
 themeChange();
 buyCakeDocComponent();
 mainNavDocComponent();
 communityDocComponents();
+exchange();
