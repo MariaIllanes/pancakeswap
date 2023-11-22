66f61f59a0533b368c855087c135efa58f99a6fd
María <m-i-y@live.com>
2023-11-22 16:06:19 +0100

    s12 footer merged

--- js/index.js
+++ js/index.js
@@ -443,3 +443,19 @@ function communityAutoShow() {
 showCommunityCard(1);
 
 setInterval(communityAutoShow, 6000);
+
+
+
+//modal
+
+function selectTransactionSpeed(speedNumber) {
+  document.querySelectorAll('.transaction-speed-btn').forEach(button => {
+    button.classList.remove('selected');
+});
+
+  document.getElementById('speedBtn' + speedNumber).classList.add('selected');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  selectTransactionSpeed(1);
+});
