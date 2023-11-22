7c9cd4c2148e1daab7c7e2c79ce91ab0fa552888
María <m-i-y@live.com>
2023-11-22 13:14:14 +0100

    feat: hmtl to js. ready to merge

--- js/index.js
+++ js/index.js
@@ -337,3 +337,76 @@ gamesNFTCard.addEventListener('mouseout', function () {
 
 ecosystemGamesNFTContainer.appendChild(gamesNFTCard);
 });
+
+
+
+
+// section "exchange stats"
+
+const countElementStat1 = document.getElementById("count-circulating");
+
+function countCirculating() {
+    let count = 0;
+    const interval = 1;
+    const target = 224752258;
+   
+    function updateCountStat1() {
+        countElementStat1.textContent = count;
+        count+= 2000000;
+        if (count <= target) {
+            setTimeout(updateCountStat1, interval);
+      }
+   }
+
+   updateCountStat1();
+}
+
+countCirculating();
+
+
+
+
+
+
+const countElementStat2 = document.getElementById("count-total-supply");
+
+function countTotalSupply() {
+    let count = 0;
+    const interval = 1;
+    const target = 387913445;
+   
+    function updateCountStat2() {
+        countElementStat2.textContent = count;
+        count+= 2500000;
+        if (count <= target) {
+            setTimeout(updateCountStat2, interval);
+      }
+   }
+
+   updateCountStat2();
+}
+
+countTotalSupply();
+
+
+
+
+const countElementStat3 = document.getElementById("count-token-burn");
+
+function countTokenSupply() {
+    let count = 0;
+    const interval = 1;
+    const target = 1027180086;
+   
+    function updateCountStat3() {
+        countElementStat3.textContent = count;
+        count+= 10000000;
+        if (count <= target) {
+            setTimeout(updateCountStat3, interval);
+      }
+   }
+
+   updateCountStat3();
+}
+
+countTokenSupply();
