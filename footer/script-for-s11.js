314f708ebd5dc1490b431c7bfce6ce611eca89d5
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-11-26 20:53:11 +0100

    Fixing main in local.

--- footer/script-for-s11.js
+++ footer/script-for-s11.js
@@ -1,5 +1,7 @@
 const footerBannerButton = document.getElementById("button-s11");
-const hiddenFooterBanner = document.querySelector("#footer-banner-hidden-section");
+const hiddenFooterBanner = document.querySelector(
+  "#footer-banner-hidden-section"
+);
 
 let footerBannerisHidden = true;
 
@@ -9,13 +11,18 @@ footerBannerButton.addEventListener("click", () => {
   } else {
     hiddenFooterBanner.style.display = "none";
   }
-  
-  footerBannerisHidden = !footerBannerisHidden; 
-  console.log("Show button clicked. footerBannerisHidden:", footerBannerisHidden);
+
+  footerBannerisHidden = !footerBannerisHidden;
+  console.log(
+    "Show button clicked. footerBannerisHidden:",
+    footerBannerisHidden
+  );
 });
 
 document.addEventListener("click", (event) => {
-  const isClickedOutside = !hiddenFooterBanner.contains(event.target) && event.target !== footerBannerButton;
+  const isClickedOutside =
+    !hiddenFooterBanner.contains(event.target) &&
+    event.target !== footerBannerButton;
   if (isClickedOutside && !footerBannerisHidden) {
     hiddenFooterBanner.style.display = "none";
     footerBannerisHidden = true;
@@ -23,83 +30,72 @@ document.addEventListener("click", (event) => {
   }
 });
 
-
-
-
-
-
-
-
-let footerBannerIndex = 1; 
+let footerBannerIndex = 1;
 
 function showFooterBannerCard(index) {
-    const footerBannerSlides = document.querySelectorAll('.footer-card-slide');
-    const footerBannerButtons = document.querySelectorAll('.footer-slideshow-buttons button');
+  const footerBannerSlides = document.querySelectorAll(".footer-card-slide");
+  const footerBannerButtons = document.querySelectorAll(
+    ".footer-slideshow-buttons button"
+  );
 
-    footerBannerSlides.forEach(slide => {
-        slide.style.display = 'none';
-    });
-    footerBannerSlides.forEach(button => {
-        button.classList.remove('active');
-    });
+  footerBannerSlides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  footerBannerSlides.forEach((button) => {
+    button.classList.remove("active");
+  });
 
-    footerBannerSlides[index - 1].style.display = 'block';
-    footerBannerButtons[index - 1].classList.add('active');
-    footerBannerIndex = index;
+  footerBannerSlides[index - 1].style.display = "block";
+  footerBannerButtons[index - 1].classList.add("active");
+  footerBannerIndex = index;
 }
 
 function footerBannerAutoShow() {
-    const footerBannerSlides = document.querySelectorAll('.footer-card-slide');
-    const footerBannerButtons = document.querySelectorAll('.footer-slideshow-buttons button');
+  const footerBannerSlides = document.querySelectorAll(".footer-card-slide");
+  const footerBannerButtons = document.querySelectorAll(
+    ".footer-slideshow-buttons button"
+  );
 
-    footerBannerSlides.forEach(slide => {
-        slide.style.display = 'none';
-    });
-    footerBannerButtons.forEach(button => {
-        button.classList.remove('active');
-    });
+  footerBannerSlides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  footerBannerButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
 
-    footerBannerIndex++;
-    if (footerBannerIndex > footerBannerSlides.length) {
-      footerBannerIndex = 1;
-    }
+  footerBannerIndex++;
+  if (footerBannerIndex > footerBannerSlides.length) {
+    footerBannerIndex = 1;
+  }
 
-    footerBannerSlides[footerBannerIndex - 1].style.display = 'block';
-    footerBannerButtons[footerBannerIndex - 1].classList.add('active');
+  footerBannerSlides[footerBannerIndex - 1].style.display = "block";
+  footerBannerButtons[footerBannerIndex - 1].classList.add("active");
 }
 
 showFooterBannerCard(1);
 
 setInterval(footerBannerAutoShow, 6000);
 
-
-
-
-
-
-
 // s11 hidden section web3 tab -------------------------------------------------------------
 
-const seeMoreButton = document.getElementById('see-more-cards-btn');
-const hiddenCardsSection = document.querySelector('.cw-more-cards');
-const cwCardsContainer = document.querySelector('.cw-cards');
+const seeMoreButton = document.getElementById("see-more-cards-btn");
+const hiddenCardsSection = document.querySelector(".cw-more-cards");
+const cwCardsContainer = document.querySelector(".cw-cards");
 
-seeMoreButton.addEventListener('click', function() {
-  seeMoreButton.style.display = 'none';
-  hiddenCardsSection.classList.add('show-more');
-  cwCardsContainer.classList.add('show-more');
+seeMoreButton.addEventListener("click", function () {
+  seeMoreButton.style.display = "none";
+  hiddenCardsSection.classList.add("show-more");
+  cwCardsContainer.classList.add("show-more");
 });
 
 function showCard(cardNumber) {
-	var cardSlides = document.querySelectorAll('.card-slide');
-	for (var i = 0; i < cardSlides.length; i++) {
-	  cardSlides[i].style.display = 'none';
-	}
-  
-	var selectedCard = document.querySelector('.card' + cardNumber);
-	if (selectedCard) {
-	  selectedCard.style.display = 'block';
-	}
+  var cardSlides = document.querySelectorAll(".card-slide");
+  for (var i = 0; i < cardSlides.length; i++) {
+    cardSlides[i].style.display = "none";
   }
-  
 
+  var selectedCard = document.querySelector(".card" + cardNumber);
+  if (selectedCard) {
+    selectedCard.style.display = "block";
+  }
+}
