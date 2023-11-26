b52975ad8d0a478029d94cc925c59641f3b7bb2a
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-11-26 20:15:14 +0100

    FIXED.

--- js/index.js
+++ js/index.js
@@ -4,34 +4,187 @@ import { communityDocComponents } from "./community.js";
 import { partnersAnimationDocComponent } from "./partners-animation.js";
 import { exchange } from "./script-for-s4.js";
 import { adCarousselDocComponent } from "./ad-caroussel.js";
-import { exchangeStatsDocComponents } from "./exchange-stats2.js";
 import { buyCakeDocComponent } from "./buy-cake.js";
 import { mainBannerDocComponent } from "./main-banner.js";
 import { themeChange } from "./themchange.js";
+import { bottomNav } from "./sec14-bottom-nav.js";
 import { arrowButton2 } from "./arrow-button.js";
 import { newsDocComponents } from "./news.js";
-import {footerBannerDocComponent} from "./footer-banner.js";
-import {footerDocComponents} from "./footer.js";
+import { footerBannerDocComponent } from "./footer-banner.js";
+import { footerDocComponents } from "./footer.js";
 import { themeChangeFooter } from "./themchange.js";
 
-
-
-
-exchangeStatsDocComponents();
-themeChangeFooter();
-newsDocComponents();
-adCarousselDocComponent();
-communityDocComponents();
+mainNavDocComponent();
 ecosystemDocComponent();
+communityDocComponents();
 partnersAnimationDocComponent();
+exchange();
+adCarousselDocComponent();
+buyCakeDocComponent();
 mainBannerDocComponent();
 themeChange();
-buyCakeDocComponent();
-mainNavDocComponent();
-exchange();
+bottomNav();
 arrowButton2();
+newsDocComponents();
 footerBannerDocComponent();
 footerDocComponents();
+themeChangeFooter();
+
+// ---------------- SEC14-REAC-NAV-BOTTOM -------------------//
+
+// Para que salga el menú del segundo icono
+
+var optionTwo = document.querySelector(".option-two");
+var menuTwo = document.querySelector(".menu-two");
+
+var timeoutId;
+
+optionTwo.addEventListener("mouseover", function () {
+  clearTimeout(timeoutId); // Limpiar cualquier tiempo de espera pendiente
+  showMenu();
+});
+
+optionTwo.addEventListener("mouseout", function () {
+  timeoutId = setTimeout(hideMenu, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+menuTwo.addEventListener("mouseover", keepMenuVisible);
+
+menuTwo.addEventListener("mouseout", function () {
+  timeoutId = setTimeout(hideMenu, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+function showMenu() {
+  menuTwo.style.visibility = "visible";
+  menuTwo.style.pointerEvents = "auto";
+}
+
+function keepMenuVisible() {
+  clearTimeout(timeoutId); // Limpiar cualquier tiempo de espera pendiente
+  menuTwo.style.visibility = "visible";
+  menuTwo.style.pointerEvents = "auto";
+}
+
+function hideMenu() {
+  menuTwo.style.visibility = "hidden";
+  menuTwo.style.pointerEvents = "none";
+}
+
+// Para que salga el menú del tercer icono
+
+var optionThree = document.querySelector(".option-three");
+var menuThree = document.querySelector(".menu-three");
+
+var timeoutIdThree;
+
+optionThree.addEventListener("mouseover", function () {
+  clearTimeout(timeoutIdThree); // Limpiar cualquier tiempo de espera pendiente
+  showMenuThree();
+});
+
+optionThree.addEventListener("mouseout", function () {
+  timeoutIdThree = setTimeout(hideMenuThree, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+menuThree.addEventListener("mouseover", keepMenuVisibleThree);
+
+menuThree.addEventListener("mouseout", function () {
+  timeoutIdThree = setTimeout(hideMenuThree, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+function showMenuThree() {
+  menuThree.style.visibility = "visible";
+  menuThree.style.pointerEvents = "auto";
+}
+
+function keepMenuVisibleThree() {
+  clearTimeout(timeoutIdThree); // Limpiar cualquier tiempo de espera pendiente
+  menuThree.style.visibility = "visible";
+  menuThree.style.pointerEvents = "auto";
+}
+
+function hideMenuThree() {
+  menuThree.style.visibility = "hidden";
+  menuThree.style.pointerEvents = "none";
+}
+
+// Para que salga el menú del cuarto icono
+
+var optionFour = document.querySelector(".option-four");
+var menuFour = document.querySelector(".menu-four");
+
+var timeoutIdFour;
+
+optionFour.addEventListener("mouseover", function () {
+  clearTimeout(timeoutIdFour); // Limpiar cualquier tiempo de espera pendiente
+  showMenuFour();
+});
+
+optionFour.addEventListener("mouseout", function () {
+  timeoutIdFour = setTimeout(hideMenuFour, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+menuFour.addEventListener("mouseover", keepMenuVisibleFour);
+
+menuFour.addEventListener("mouseout", function () {
+  timeoutIdFour = setTimeout(hideMenuFour, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+function showMenuFour() {
+  menuFour.style.visibility = "visible";
+  menuFour.style.pointerEvents = "auto";
+}
+
+function keepMenuVisibleFour() {
+  clearTimeout(timeoutIdFour); // Limpiar cualquier tiempo de espera pendiente
+  menuFour.style.visibility = "visible";
+  menuFour.style.pointerEvents = "auto";
+}
+
+function hideMenuFour() {
+  menuFour.style.visibility = "hidden";
+  menuFour.style.pointerEvents = "none";
+}
+
+// Para que salga el menú del quinto icono
+
+var optionFive = document.querySelector(".option-five");
+var menuFive = document.querySelector(".menu-five");
+
+var timeoutIdFive;
+
+optionFive.addEventListener("mouseover", function () {
+  clearTimeout(timeoutIdFive); // Limpiar cualquier tiempo de espera pendiente
+  showMenuFive();
+});
+
+optionFive.addEventListener("mouseout", function () {
+  timeoutIdFive = setTimeout(hideMenuFive, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+menuFive.addEventListener("mouseover", keepMenuVisibleFive);
+
+menuFive.addEventListener("mouseout", function () {
+  timeoutIdFive = setTimeout(hideMenuFive, 300); // Establecer un tiempo de espera antes de ocultar el menú
+});
+
+function showMenuFive() {
+  menuFive.style.visibility = "visible";
+  menuFive.style.pointerEvents = "auto";
+}
+
+function keepMenuVisibleFive() {
+  clearTimeout(timeoutIdFive); // Limpiar cualquier tiempo de espera pendiente
+  menuFive.style.visibility = "visible";
+  menuFive.style.pointerEvents = "auto";
+}
+
+function hideMenuFive() {
+  menuFive.style.visibility = "hidden";
+  menuFive.style.pointerEvents = "none";
+}
+
+// ------- FIN DE SEC14-REAC-NAV-BOTTOM -------
 
 const arrowButton = document.querySelector(".other-element-button");
 const buttonContainer = document.querySelector(".other-element-container");
@@ -477,7 +630,6 @@ arrowLeft.addEventListener("click", () => {
   }
 });
 
-
 // SEC 8 Exchange stats
 // Función para desplazarse a la derecha
 arrowRight.addEventListener("click", () => {
@@ -549,12 +701,12 @@ showCommunityCard(1);
 
 setInterval(communityAutoShow, 6000);
 
-
-
 // footer banner
 
 const footerBannerButton = document.getElementById("button-s11");
-const hiddenFooterBanner = document.querySelector("#footer-banner-hidden-section");
+const hiddenFooterBanner = document.querySelector(
+  "#footer-banner-hidden-section"
+);
 
 let footerBannerisHidden = true;
 
@@ -564,13 +716,18 @@ footerBannerButton.addEventListener("click", () => {
   } else {
     hiddenFooterBanner.style.display = "none";
   }
-  
-  footerBannerisHidden = !footerBannerisHidden; 
-  console.log("Show button clicked. footerBannerisHidden:", footerBannerisHidden);
+
+  footerBannerisHidden = !footerBannerisHidden;
+  console.log(
+    "Show button clicked. footerBannerisHidden:",
+    footerBannerisHidden
+  );
 });
 
 document.addEventListener("click", (event) => {
-  const isClickedOutside = !hiddenFooterBanner.contains(event.target) && event.target !== footerBannerButton;
+  const isClickedOutside =
+    !hiddenFooterBanner.contains(event.target) &&
+    event.target !== footerBannerButton;
   if (isClickedOutside && !footerBannerisHidden) {
     hiddenFooterBanner.style.display = "none";
     footerBannerisHidden = true;
@@ -578,144 +735,137 @@ document.addEventListener("click", (event) => {
   }
 });
 
-
-
-
-
-
-
-
-let footerBannerIndex = 1; 
+let footerBannerIndex = 1;
 
 function showFooterBannerCard(index) {
-    const footerBannerSlides = document.querySelectorAll('.footer-card-slide');
-    const footerBannerButtons = document.querySelectorAll('.footer-slideshow-buttons button');
+  const footerBannerSlides = document.querySelectorAll(".footer-card-slide");
+  const footerBannerButtons = document.querySelectorAll(
+    ".footer-slideshow-buttons button"
+  );
 
-    footerBannerSlides.forEach(slide => {
-        slide.style.display = 'none';
-    });
-    footerBannerSlides.forEach(button => {
-        button.classList.remove('active');
-    });
+  footerBannerSlides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  footerBannerSlides.forEach((button) => {
+    button.classList.remove("active");
+  });
 
-    footerBannerSlides[index - 1].style.display = 'block';
-    footerBannerButtons[index - 1].classList.add('active');
-    footerBannerIndex = index;
+  footerBannerSlides[index - 1].style.display = "block";
+  footerBannerButtons[index - 1].classList.add("active");
+  footerBannerIndex = index;
 }
 
 function footerBannerAutoShow() {
-    const footerBannerSlides = document.querySelectorAll('.footer-card-slide');
-    const footerBannerButtons = document.querySelectorAll('.footer-slideshow-buttons button');
+  const footerBannerSlides = document.querySelectorAll(".footer-card-slide");
+  const footerBannerButtons = document.querySelectorAll(
+    ".footer-slideshow-buttons button"
+  );
 
-    footerBannerSlides.forEach(slide => {
-        slide.style.display = 'none';
-    });
-    footerBannerButtons.forEach(button => {
-        button.classList.remove('active');
-    });
+  footerBannerSlides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  footerBannerButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
 
-    footerBannerIndex++;
-    if (footerBannerIndex > footerBannerSlides.length) {
-      footerBannerIndex = 1;
-    }
+  footerBannerIndex++;
+  if (footerBannerIndex > footerBannerSlides.length) {
+    footerBannerIndex = 1;
+  }
 
-    footerBannerSlides[footerBannerIndex - 1].style.display = 'block';
-    footerBannerButtons[footerBannerIndex - 1].classList.add('active');
+  footerBannerSlides[footerBannerIndex - 1].style.display = "block";
+  footerBannerButtons[footerBannerIndex - 1].classList.add("active");
 }
 
 showFooterBannerCard(1);
 
 setInterval(footerBannerAutoShow, 6000);
 
-
 // s11 hidden section web3 tab -------------------------------------------------------------
 
-const seeMoreButton = document.getElementById('see-more-cards-btn');
-const hiddenCardsSection = document.querySelector('.cw-more-cards');
-const cwCardsContainer = document.querySelector('.cw-cards');
+const seeMoreButton = document.getElementById("see-more-cards-btn");
+const hiddenCardsSection = document.querySelector(".cw-more-cards");
+const cwCardsContainer = document.querySelector(".cw-cards");
 
-seeMoreButton.addEventListener('click', function() {
-  seeMoreButton.style.display = 'none';
-  hiddenCardsSection.classList.add('show-more');
-  cwCardsContainer.classList.add('show-more');
+seeMoreButton.addEventListener("click", function () {
+  seeMoreButton.style.display = "none";
+  hiddenCardsSection.classList.add("show-more");
+  cwCardsContainer.classList.add("show-more");
 });
 
 function showCard(cardNumber) {
-	var cardSlides = document.querySelectorAll('.card-slide');
-	for (var i = 0; i < cardSlides.length; i++) {
-	  cardSlides[i].style.display = 'none';
-	}
-  
-	var selectedCard = document.querySelector('.card' + cardNumber);
-	if (selectedCard) {
-	  selectedCard.style.display = 'block';
-	}
+  var cardSlides = document.querySelectorAll(".card-slide");
+  for (var i = 0; i < cardSlides.length; i++) {
+    cardSlides[i].style.display = "none";
   }
 
+  var selectedCard = document.querySelector(".card" + cardNumber);
+  if (selectedCard) {
+    selectedCard.style.display = "block";
+  }
+}
 
-  // footer 
-  const svgElement = document.getElementById('logo-telegram');
-const customOptions = document.getElementById('telegram-language-options');
+// footer
+const svgElement = document.getElementById("logo-telegram");
+const customOptions = document.getElementById("telegram-language-options");
 let menuVisible = false;
 
-svgElement.addEventListener('mouseenter', function() {
-    customOptions.style.display = 'block';
-    menuVisible = true;
+svgElement.addEventListener("mouseenter", function () {
+  customOptions.style.display = "block";
+  menuVisible = true;
 });
 
-customOptions.addEventListener('mouseleave', function() {
-    menuVisible = false;
-    setTimeout(() => {
-        if (!menuVisible) {
-            customOptions.style.display = 'none';
-        }
-    }, 1000);
-});
-
-customOptions.addEventListener('click', function(e) {
-    if (e.target.tagName === 'LI') {
-        const selectedValue = e.target.getAttribute('data-value');
-        location.href = selectedValue; 
+customOptions.addEventListener("mouseleave", function () {
+  menuVisible = false;
+  setTimeout(() => {
+    if (!menuVisible) {
+      customOptions.style.display = "none";
     }
+  }, 1000);
+});
+
+customOptions.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    const selectedValue = e.target.getAttribute("data-value");
+    location.href = selectedValue;
+  }
 });
 
 // Hide menu when clicking outside of it
-document.addEventListener('click', function(e) {
-    const isClickInside = svgElement.contains(e.target) || customOptions.contains(e.target);
-    if (!isClickInside) {
-        customOptions.style.display = 'none';
-        menuVisible = false;
-    }
+document.addEventListener("click", function (e) {
+  const isClickInside =
+    svgElement.contains(e.target) || customOptions.contains(e.target);
+  if (!isClickInside) {
+    customOptions.style.display = "none";
+    menuVisible = false;
+  }
 });
-
-
-
-
 
 // language button
 
-const worldSvgElement = document.getElementById('select-language-web-label');
-const languageOptions = document.getElementById('footer-language-options');
+const worldSvgElement = document.getElementById("select-language-web-label");
+const languageOptions = document.getElementById("footer-language-options");
 let languageMenu = false;
 
-worldSvgElement.addEventListener('mouseenter', function() {
-    languageOptions.style.display = 'block';
-    languageMenu = true;
+worldSvgElement.addEventListener("mouseenter", function () {
+  languageOptions.style.display = "block";
+  languageMenu = true;
 });
 
-languageOptions.addEventListener('mouseleave', function() {
-    languageMenu = false;
-    setTimeout(() => {
-        if (!languageMenu) {
-            languageOptions.style.display = 'none';
-        }
-    }, 1000);
-});
-
-document.addEventListener('click', function(e) {
-    const isClickInsideLanguage = worldSvgElement.contains(e.target) || customOptions.contains(e.target);
-    if (!isClickInsideLanguage) {
-        languageOptions.style.display = 'none';
-        languageMenu = false;
+languageOptions.addEventListener("mouseleave", function () {
+  languageMenu = false;
+  setTimeout(() => {
+    if (!languageMenu) {
+      languageOptions.style.display = "none";
     }
+  }, 1000);
+});
+
+document.addEventListener("click", function (e) {
+  const isClickInsideLanguage =
+    worldSvgElement.contains(e.target) || customOptions.contains(e.target);
+  if (!isClickInsideLanguage) {
+    languageOptions.style.display = "none";
+    languageMenu = false;
+  }
 });
